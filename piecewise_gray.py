1e061bddd556327df94be7b6aa889d0222576d3a
Laurence Burden <laurenceburden@gmail.com>
2025-04-07 12:37:14 -0500

    initial commit

---
+++ piecewise_gray.py
@@ -0,0 +1,10 @@
+##########################################################
+# Author: Laurence Burden
+# Date: 20250407
+# Class: Digital Image Processing
+# Professor: Dr. Roya Choupani
+##########################################################
+
+import cv2 as cv
+import pandas as pd
+
